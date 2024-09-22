3d4ba741f7ab449d30adbb3ed049441497a71cc1
Hafiz Unijaya <hafizcoolman@gmail.com>
2024-09-23 05:34:33 +0800

    Added file-pond package and new component

---
+++ src/components/ImageUpload.vue
@@ -0,0 +1,61 @@
+<template>
+  <file-pond
+    name="test"
+    ref="pond"
+    credits="false"
+    label-idle="Drop files here..."
+    :accepted-file-types="acceptedFileTypes"
+    :files="internalFiles"
+    @init="handleFilePondInit"
+    @updatefiles="onFileUpdate"
+  />
+</template>
+
+<script setup>
+
+import { ref } from 'vue';
+
+// Import Vue FilePond and plugins
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import vueFilePond from "vue-filepond";
+
+// Import CSS
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css";
+import "filepond/dist/filepond.min.css";
+
+// Create component
+const FilePond = vueFilePond(
+  FilePondPluginFileValidateType,
+  FilePondPluginImagePreview
+);
+
+// Define props
+const props = defineProps({
+  modelValue: {
+    type: Array,
+    default: () => []
+  },
+  acceptedFileTypes: {
+    type: Array,
+    default: () => ['image/jpeg', 'image/png']
+  },
+});
+
+// Custom event handlers
+const handleFilePondInit = () => {
+  console.log("FilePond has initialized");
+};
+
+
+// Reactive state for the files inside the component
+const internalFiles = ref(props.modelValue || []);
+const emit = defineEmits(['update:modelValue']);
+const onFileUpdate = (fileItems) => {
+  console.log("onfileupdate");
+  internalFiles.value = fileItems.map(item => item.file);
+  emit('update:modelValue', internalFiles.value);
+  console.log(internalFiles.value);
+};
+
+</script>
