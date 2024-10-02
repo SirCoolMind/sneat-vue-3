807ff73e010e4be6d0f4bdc441199ead42618d43
Hafiz Unijaya <hafizcoolman@gmail.com>
2024-10-03 04:31:39 +0800

    [Navbar] Improve sidebar by turning into json

--- src/layouts/components/NavItems.vue
+++ src/layouts/components/NavItems.vue
@@ -1,133 +1,108 @@
+<template>
+  <div v-for="item in navItems" :key="item.title">
+    <!-- Check if item has children (sub-items) to render as VerticalNavGroup -->
+    <VerticalNavGroup v-if="item.children" :item="item">
+      <VerticalNavLink v-for="child in item.children" :key="child.title" :item="child" />
+    </VerticalNavGroup>
+    
+    <!-- Check if item is a section title -->
+    <VerticalNavSectionTitle v-else-if="item.heading" :item="item" />
+    
+    <!-- Render a single VerticalNavLink -->
+    <VerticalNavLink v-else :item="item" />
+  </div>
+</template>
+
+<style>
+  .layout-nav-type-vertical .layout-vertical-nav .nav-section-title {
+    margin-block-end: 0.8rem;
+    margin-block-start: 0.8rem;
+  }
+</style>
+
 <script setup>
 import VerticalNavSectionTitle from '@/@layouts/components/VerticalNavSectionTitle.vue';
 import VerticalNavGroup from '@layouts/components/VerticalNavGroup.vue';
 import VerticalNavLink from '@layouts/components/VerticalNavLink.vue';
-</script>
 
-<template>
-  <!-- 👉 Dashboards -->
-  <VerticalNavGroup
-    :item="{
-      title: 'Dashboards',
-      badgeContent: '5',
-      badgeClass: 'bg-error',
-      icon: 'bx-home-smile',
-    }"
-  >
-    <VerticalNavLink
-      :item="{
+const navItems = [
+  {
+    title: 'Dashboards',
+    badgeContent: '5',
+    badgeClass: 'bg-error',
+    icon: 'bx-home-smile',
+    children: [
+      {
         title: 'Analytics',
         to: '/dashboard',
-      }"
-    />
-  </VerticalNavGroup>
-
-  <VerticalNavSectionTitle
-    :item="{
-      heading: 'Apps',
-    }"
-  />
-
-  <VerticalNavLink
-    :item="{
-      title: 'KPOP Collection',
-      icon: 'bx-collection',
-      to: '/kpop-collection',
-    }"
-  />
-
-  <!-- 👉 Apps & Pages -->
-  <VerticalNavLink
-    :item="{
-      title: 'Account Settings',
-      icon: 'bx-user',
-      to: '/account-settings',
-    }"
-  />
-
-  <VerticalNavLink
-    :item="{
-      title: 'Login',
-      icon: 'bx-log-in',
-      to: '/login',
-    }"
-  />
-  <VerticalNavLink
-    :item="{
-      title: 'Register',
-      icon: 'bx-user-plus',
-      to: '/register',
-    }"
-  />
-  <VerticalNavLink
-    :item="{
-      title: 'Error',
-      icon: 'bx-info-circle',
-      to: '/no-existence',
-    }"
-  />
-
-  <!-- 👉 User Interface -->
-  <VerticalNavSectionTitle
-    :item="{
-      heading: 'User Interface',
-    }"
-  />
-  <VerticalNavLink
-    :item="{
-      title: 'Typography',
-      icon: 'bx-text',
-      to: '/typography',
-    }"
-  />
-  <VerticalNavLink
-    :item="{
-      title: 'Icons',
-      icon: 'bx-package',
-      to: '/icons',
-    }"
-  />
-  <VerticalNavLink
-    :item="{
-      title: 'Cards',
-      icon: 'bx-credit-card',
-      to: '/cards',
-    }"
-  />
-
-  <!-- 👉 Forms & Tables -->
-  <VerticalNavSectionTitle
-    :item="{
-      heading: 'Forms & Tables',
-    }"
-  />
-  <VerticalNavLink
-    :item="{
-      title: 'Form Layouts',
-      icon: 'bx-layout',
-      to: '/form-layouts',
-    }"
-  />
-  <VerticalNavLink
-    :item="{
-      title: 'Tables',
-      icon: 'bx-table',
-      to: '/tables',
-    }"
-  />
-
-  <!-- 👉 Others -->
-  <VerticalNavSectionTitle
-    :item="{
-      heading: 'Others',
-    }"
-  />
-  <VerticalNavLink
-    :item="{
-      title: 'Documentation',
-      icon: 'bx-file',
-      href: 'https://demos.themeselection.com/sneat-vuetify-vuejs-admin-template/documentation/',
-      target: '_blank',
-    }"
-  />
-</template>
+      }
+    ]
+  },
+  {
+    heading: 'Apps',
+  },
+  {
+    heading: "Templates Things"
+  },
+  {
+    title: "Account Settings",
+    icon: "bx-user",
+    to: "/account-settings"
+  },
+  {
+    title: "Login",
+    icon: "bx-log-in",
+    to: "/login"
+  },
+  {
+    title: "Register",
+    icon: "bx-user-plus",
+    to: "/register"
+  },
+  {
+    title: "Error",
+    icon: "bx-info-circle",
+    to: "/no-existence"
+  },
+  {
+    heading: "User Interface"
+  },
+  {
+    title: "Typography",
+    icon: "bx-text",
+    to: "/typography"
+  },
+  {
+    title: "Icons",
+    icon: "bx-package",
+    to: "/icons"
+  },
+  {
+    title: "Cards",
+    icon: "bx-credit-card",
+    to: "/cards"
+  },
+  {
+    heading: "Forms & Tables"
+  },
+  {
+    title: "Form Layouts",
+    icon: "bx-layout",
+    to: "/form-layouts"
+  },
+  {
+    title: "Tables",
+    icon: "bx-table",
+    to: "/tables"
+  },
+  {
+    heading: "Others"
+  },
+  {
+    title: "Documentation",
+    icon: "bx-file",
+    "href": "https://demos.themeselection.com/sneat-vuetify-vuejs-admin-template/documentation/",
+    "target": "_blank"
+  },
+]
+</script>
