e906702d5a8e5e48f1f66f4e6cab641a77b6cd3a
Hafiz Unijaya <hafizcoolman@gmail.com>
2024-12-02 05:54:29 +0800

    [Deploy] Add remove folder dist before unzip

--- deploy-dist.bat
+++ deploy-dist.bat
@@ -2,7 +2,7 @@
 set SERVER_USER=root
 set SERVER_IP=hafiz.day
 set SERVER_DIR=/var/www/sneat-vue-3
-set LOCAL_FILE=dist.tar.gz
+set LOCAL_FILE=vuetemplate.tar.gz
 
 :: Generate date in DDMMYYYY format using PowerShell
 for /f %%I in ('powershell -NoProfile -Command "(Get-Date).ToString('ddMMyyyy')"') do set DATE=%%I
@@ -16,7 +16,7 @@ echo Uploading %LOCAL_FILE% to %SERVER_USER%@%SERVER_IP%:%SERVER_DIR%
 scp %LOCAL_FILE% %SERVER_USER%@%SERVER_IP%:%SERVER_DIR%
 
 echo Extracting %LOCAL_FILE% on server and creating backup
-ssh %SERVER_USER%@%SERVER_IP% "cd %SERVER_DIR%  && if [ ! -f %BACKUP_FILE%.tar.gz ]; then echo Backup file %BACKUP_FILE% created; tar -czf %BACKUP_FILE%.tar.gz dist; else echo Backup file %BACKUP_FILE% already exists. Skipping backup; fi && tar xzvf %LOCAL_FILE% > /dev/null 2>&1"
+ssh %SERVER_USER%@%SERVER_IP% "cd %SERVER_DIR%  && if [ ! -f %BACKUP_FILE%.tar.gz ]; then echo Backup file %BACKUP_FILE% created; tar -czf %BACKUP_FILE%.tar.gz dist; else echo Backup file %BACKUP_FILE% already exists. Skipping backup; fi && rm -r dist && tar xzvf %LOCAL_FILE% > /dev/null 2>&1"
 
 echo Deployment complete!
 exit
