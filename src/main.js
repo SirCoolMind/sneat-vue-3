7b83653908d3b8925fe4b841ebcc3c3087882439
Hafiz Unijaya <hafizcoolman@gmail.com>
2024-12-25 02:18:25 +0800

    Added NProgress and implement loading for every router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
 import App from '@/App.vue'
 import { registerPlugins } from '@core/utils/plugins'
+import { createApp } from 'vue'
 
 // Styles
 import '@core/scss/template/index.scss'
@@ -9,6 +9,11 @@ import '@styles/styles.scss'
 
 // Create vue app
 const app = createApp(App)
+
+// NProgress (loading bar) and make it globally available
+import NProgress from 'nprogress'
+import '../node_modules/nprogress/nprogress.css'
+app.config.globalProperties.$nprogress = NProgress;
 
 
 // Register plugins
--- src/plugins/router/index.js
+++ src/plugins/router/index.js
@@ -1,12 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { routes } from './routes'
+import NProgress from 'nprogress';
+import { createRouter, createWebHistory } from 'vue-router';
+import { routes } from './routes';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
 
+router.beforeEach((to, from, next) => {
+  NProgress.start(); //Start loading
+  next()
+})
+
+router.afterEach(() => {
+  NProgress.done(); //Finish loading
+})
+
 export default function (app) {
   app.use(router)
 }
-export { router }
+export { router };
+
