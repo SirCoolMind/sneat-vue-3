3416c4ae7919959ff4fd39f5b5552db096809727
hafiz <hafizcoolman@gmail.com>
2025-01-16 14:54:04 +0800

    Add flex end for button

--- src/components/CardHeaderDefault.vue
+++ src/components/CardHeaderDefault.vue
@@ -2,11 +2,11 @@
   <VCardItem class="py-4 mb-4" style="border-bottom: 1px solid gray;">
     <VCardTitle>
       <VContainer class="pa-0">
-        <VRow justify="space-between" align="center" dense>
-          <VCol cols="12" sm="8" class="overflow-hidden">
+        <VRow class="d-flex align-center" dense>
+          <VCol cols="9" class="overflow-hidden">
             <Breadcrumbs :items="breadcrumbs" />
           </VCol>
-          <VCol cols="auto" align-self="end">
+          <VCol cols="3" class="d-flex justify-end">
             <VFabTransition>
               <VBtn :to="buttonTo">{{ buttonText }}</VBtn>
             </VFabTransition>
